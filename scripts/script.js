d91cd2aab1c486a057f3eff6814a161a86f118c7
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-11-01 17:49:56 +0530

    Finally Working

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	// Array of Users who regularly stream on Twitch
 	var users = [
 					"ESL_SC2",
@@ -10,31 +11,41 @@ $(document).ready(function(){
 					"RobotCaleb",
 					"noobs2ninjas"
 				];
-
- 
-
-         var $output = "";
-   	
+         
+   		var output = '';
     	// Loop through each user to get data
-    	$.each(users, function(key, val) {
-    	  var $url = 'https://wind-bow.glitch.me/twitch-api/channels/' + users[key] + '?callback=?';
-	      	$output += '<li>';
-
+    	for (i = 0; i < users.length; i++) { 
+    		
+    		var $url = 'https://wind-bow.glitch.me/twitch-api/channels/' + users[i] + '?callback=?';
+	     	
 	    	$.getJSON($url, function(data) {
-	    		
-	    		$output += '<img src="' + data.logo +'">';
-	    		console.log($output);
-	    	if(data.stream === null){
-	    		
-	       	} else {
+	    			console.log(data);
+	    	if(data.status == null){
+	    		if(data.logo == null){
+	    		output += '<li><img src="images/noimage.png"><span class="status">' + data.display_name + ' is Offline</span></li>';
 
-	        	}
-	       
+	       		}
+	       		else {
+	       		output += '<li><img src="' + data.logo +'"><span class="status">' + data.display_name + ' is Offline</span></li>'; 		  		   
+	       		}
+	    		// 
+	          	} else {
+	    		if(data.logo == null){
+	    	 	output += '<li><img src="images/noimage.png"><span class="status">' + data.display_name + ' is Online</span><a href="' + data.url + '" target="_blank"><span class="stream">Stream</span></a></li>';
+	    	 
+	       		}
+	       		else {
+	        	 output += '<li><img src="' + data.logo +'"><span class="status">' + data.display_name + ' is Online</span><a href="' + data.url + '" target="_blank"<span class="stream">Stream</span></a></li>';		  		   
+	        	 
+	       		}
+	    	}
+    		
+	       	// console.log($output);
+	       	$('#results').html(output); 
+	       	$('#loading').hide();
+     	  }); // getJSON
+			
+    } // For loop
 
-     	  	}); // getJSON
-	   $output += '</li>';
-    }); // Each loop
-
-
- 			$('#results').html($output);
+		
    });// document.ready
